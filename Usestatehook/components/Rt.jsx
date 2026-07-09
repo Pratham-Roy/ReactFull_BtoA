f792218c77cc5555b70105698ea240c4f0e14e1e
unknown <2k22.cse.2213369@gmail.com>
2026-07-09 12:17:47 +0530

    8th commit

--- Usestatehook/components/Rt.jsx
+++ Usestatehook/components/Rt.jsx
@@ -2,12 +2,43 @@ import React from 'react';
 import { useState } from 'react';
 
 const Rt = () => {
+    const total = 100;
     const [counter, setCounter] = useState(0);
+    const [basket1, setBasket1] = useState(100);
+    const [basket2, setBasket2] = useState(0);
+
+    const handleTransferToBasket2 = () => {
+        if (basket1 > 0) {
+            setBasket1(basket1 - 1);
+            setBasket2(basket2 + 1);
+        }
+    };
+
+    const handleTransferToBasket1 = () => {
+        if (basket2 > 0) { 
+            setBasket1(basket1 + 1);
+            setBasket2(basket2 - 1);
+        }
+    };
+
     return (
-        <>
+        <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
             <h1>Counter: {counter}</h1>
-            <button onClick={() => setCounter(counter + 1)}>Increment</button>
-        </>
+            <button onClick={() => setCounter(counter + 1)}>Increment Counter</button>
+            <hr />
+            <div>
+                <h3>Basket Management</h3>
+                <span>{basket1} basket1 </span>
+                <button onClick={handleTransferToBasket2}>
+                    Transfer to Basket 2
+                </button>
+                <br /><br />
+                <span>{basket2} basket2 </span>
+                <button onClick={handleTransferToBasket1}>
+                    Transfer to Basket 1
+                </button>
+            </div>
+        </div>
     );
 };
 export default Rt;
